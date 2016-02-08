05eedf048901839f1100f646e266ec6cfb95ff5a
bill auger <mr.j.spam.me@gmail.com>
2016-02-08 06:47:21 -0500

    add licence to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
-/*
-Loopidity MVC classes:
-Loopidity    - main  controller class (== 0                    instances)
-LoopiditySDL - main  view       class (== 0                    instances)
-Scene        - scene model      class (== NUM_SCENES           instances)
-SceneSDL     - scene view       class (== NUM_SCENES           instances)
-Loop         - loop  model      class (<= NUM_SCENES * N_LOOPS instances)
-LoopSDL      - loop  view       class (<= NUM_SCENES * N_LOOPS instances)
-JackIO       - JACK  wrapper    class (== 0                    instances)
-Trace        - debug trace      class (== 0                    instances)
-*/
+/*\ Loopidity - multitrack audio looper designed for live handsfree use
+|*| https://github.com/bill-auger/loopidity/issues/
+|*| Copyright 2013,2015 Bill Auger - https://bill-auger.github.io/
+|*|
+|*| This file is part of Loopidity.
+|*|
+|*| Loopidity is free software: you can redistribute it and/or modify
+|*| it under the terms of the GNU General Public License version 3
+|*| as published by the Free Software Foundation.
+|*|
+|*| Loopidity is distributed in the hope that it will be useful,
+|*| but WITHOUT ANY WARRANTY; without even the implied warranty of
+|*| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+|*| GNU General Public License for more details.
+|*|
+|*| You should have received a copy of the GNU General Public License
+|*| along with Loopidity.  If not, see <http://www.gnu.org/licenses/>.
+\*/
 
+/*\ Loopidity MVC classes:
+|*|  Loopidity    - main  controller class (==                    0 instances)
+|*|  LoopiditySDL - main  view       class (==                    0 instances)
+|*|  Scene        - scene model      class (==           NUM_SCENES instances)
+|*|  SceneSDL     - scene view       class (==           NUM_SCENES instances)
+|*|  Loop         - loop  model      class (<= N_LOOPS * NUM_SCENES instances)
+|*|  LoopSDL      - loop  view       class (<= N_LOOPS * NUM_SCENES instances)
+|*|  JackIO       - JACK  wrapper    class (==                    0 instances)
+|*|  Trace        - debug trace      class (==                    0 instances)
+\*/
 
-#ifdef _WIN32
-#  include <fstream>
-#endif // _WIN32
 
 #include "loopidity.h"
 
@@ -22,11 +36,11 @@ Trace        - debug trace      class (== 0                    instances)
 
 int main(int argc , char** argv)
 {
-DEBUG_TRACE_MAIN_IN
+	int exitStatus = Loopidity::Main(argc , argv) ; Loopidity::Cleanup() ;
 
-  int exitStatus = Loopidity::Main(argc , argv) ; Loopidity::Cleanup() ;
+#if DEBUG_TRACE
+if (exitStatus) cout << INIT_FAIL_MSG << endl ;
+#endif
 
-DEBUG_TRACE_MAIN_OUT
-
-  return exitStatus ;
+	return exitStatus ;
 }
